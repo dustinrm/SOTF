65b42d8aa2e7f9639f0fc1d11cc63ecd46475a1d
Dustin Marshall <dustinrm@uab.edu>
2021-03-01 10:09:40 -0600

    Added UserProfile class

---
+++ app/src/main/java/com/example/mainmenu/UserProfile.java
@@ -0,0 +1,25 @@
+package com.example.mainmenu;
+
+public class UserProfile {
+    //Attributes
+    private String username;
+    private String password;
+    private int hiddenRating;
+    //Constructors
+    public UserProfile(String username, String password){
+        this.password = password;
+        this.username = username;
+        hiddenRating = 0;
+    }
+    //Methods
+    public void increaseRating(int ratingChange){
+        this.hiddenRating += ratingChange;
+    }
+    public void decreaseRating(int ratingChange){
+        if(this.hiddenRating - ratingChange <= 0){
+            this.hiddenRating = 0;
+        }else this.hiddenRating -= ratingChange;
+    }
+    public void getUsername{return this.username}
+    public void getPassword{return this.password}
+}
